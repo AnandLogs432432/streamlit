226ae412cf5c2f6c76fbfb22a48e1b63f768b52d
AnandLogs432432 <anandlogs@gmail.com>
2024-06-23 15:16:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,8 @@ else:
 
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 
-st.dataframe(df)
+df1 = df.set_index('Sub_Category')
+st.dataframe(df1)
 
 
 st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
