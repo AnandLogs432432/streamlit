aa8907e6916f04988d9cf50a87fc6cc4a75f4526
AnandLogs432432 <anandlogs@gmail.com>
2024-06-24 19:48:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
+import plotly as px
 import math
 
 st.title("Data App Assignment, on June 20th")
